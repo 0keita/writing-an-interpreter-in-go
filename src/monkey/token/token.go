b5cce18e7e875d31089032008bc00f491ff21cc1
Keita Yoshida <tanktaex@gmail.com>
2020-07-29 21:54:01 +0900

    Improve lexer

--- src/monkey/token/token.go
+++ src/monkey/token/token.go
@@ -26,13 +26,21 @@ const (
 	LET      = "LET"
 )
 
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
+
 type TokenType string
 
 type Token struct {
 	Type    TokenType
 	Literal string
 }
-
-// func main() {
-// 	fmt.Println("vim-go")
-// }
